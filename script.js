05aa1c599d5c7e5a375770c8ad47f2f9ad139616
Canberra Roo <anaiya.badi@gmail.com>
2018-09-08 16:20:49 -0400

    hard stuff

--- script.js
+++ script.js
@@ -2,13 +2,25 @@
 $("button").click( () => {
 
    let name = $("#input1").val();
+   $("p").append(`Once there was a ${name} named `);
+
+
+
+});
+
+
+
+
+$("#make").click( () => {
+
+   let name = $("#input1").val();
    $("p").append(` ${name} is `);
 
 
 
 });
 
-$("button").click( () => {
+$("#make").click( () => {
 
    let name = $("#input2").val();
    $("p").append(`${name} years old. `);
