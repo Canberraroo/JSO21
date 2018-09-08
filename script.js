2a5fe675a043eadcd2136f3f34879b2819ac5489
Canberra Roo <anaiya.badi@gmail.com>
2018-09-08 13:30:33 -0400

    PRACTICE!
    cool stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$("button").click( () => {
+
+   let name = $("#input1").val();
+   $("p").append(` ${name} is `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input2").val();
+   $("p").append(`${name} years old. `);
