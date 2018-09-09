e2ac09cc98d9a1e31f27e9fbbaf166fe998a45ef
Canberra Roo <anaiya.badi@gmail.com>
2018-09-09 10:34:30 -0400

    link is not working!

--- script.js
+++ script.js
@@ -8,19 +8,63 @@ $("button").click( () => {
 
 });
 
+$("button").click( () => {
+
+   let name = $("#input2").val();
+   $("p").append(`${name} who lived in `);
 
 
+});
 
-$("#make").click( () => {
+$("button").click( () => {
 
-   let name = $("#input1").val();
-   $("p").append(` ${name} is `);
+   let name = $("#input3").val();
+   $("p").append(`${name}.`);
 
 
 
 });
 
-$("#make").click( () => {
+$("button").click( () => {
 
    let name = $("#input2").val();
-   $("p").append(`${name} years old. `);
+   $("p").append(`${name} had both a school enemy, `);
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input4").val();
+   $("p").append(`${name} and a pure evil one `);
+
+
+
+});
+
+$("button").click( () => {
+
+   let name = $("#input5").val();
+   $("p").append(`${name}. `);
+
+
+
+});
+
+$("button").click( () => {
+
+let name = $("#input2").val();
+$("p").append(`${name} got sorted by the sorting  `);
+
+
+
+});
+
+$("button").click( () => {
+
+let name = $("#input6").val();
+$("p").append(`${name} into Gryffindor `);
+
+
+
+});
