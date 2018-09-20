cb7c30dc3a8c0a9f14623f6059b4fead85668eb7
Canberra Roo <anaiya.badi@gmail.com>
2018-09-20 15:45:09 -0400

    suff

--- script.js
+++ script.js
@@ -1,70 +1,26 @@
 
-$("button").click( () => {
+$("button").click(function()) => {
 
    let name = $("#input1").val();
-   $("p").append(`Once there was a ${name} named `);
+   $("p").append(`${name} is `);
 
 
 
 });
 
-$("button").click( () => {
+$("button").click(function()) => {
 
    let name = $("#input2").val();
-   $("p").append(`${name} who lived in `);
+   $("p").append(`${name}years old `);
 
 
 });
 
-$("button").click( () => {
+function multiNumbers(x,y){
 
-   let name = $("#input3").val();
-   $("p").append(`${name}.`);
-
-
-
-});
-
-$("button").click( () => {
-
-   let name = $("#input2").val();
-   $("p").append(`${name} had both a school enemy, `);
-
-
-});
-
-$("button").click( () => {
-
-   let name = $("#input4").val();
-   $("p").append(`${name} and a pure evil one `);
-
-
-
-});
-
-$("button").click( () => {
-
-   let name = $("#input5").val();
-   $("p").append(`${name}. `);
-
-
-
-});
-
-$("button").click( () => {
-
-let name = $("#input2").val();
-$("p").append(`${name} got sorted by the sorting  `);
-
-
-
-});
-
-$("button").click( () => {
-
-let name = $("#input6").val();
-$("p").append(`${name} into Gryffindor `);
-
-
-
-});
+return(x*y)
+}
+let x= 2
+let y=1
+let z= 0
+z=multiNumbers(x,y)
